e6a279b03160d53bba8fb70969d5759932d8e904
seattlekiran <seattlekiran@gmail.com>
2012-10-18 15:56:53 -0700

    rearranged the model diagram elements for easy read

--- ConsoleApplication1/Models/Model1.Designer.cs
+++ ConsoleApplication1/Models/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'C:\Users\kichalla\documents\visual studio 2012\Projects\ConsoleApplication1\ConsoleApplication1\Models\Model1.edmx'. 
+﻿// Default code generation is disabled for model 'D:\GitHub\NorthwindSample\ConsoleApplication1\Models\Model1.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
